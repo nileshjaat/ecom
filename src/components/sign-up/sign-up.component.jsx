ead84fe0c9228e6d2d5e5f576e9d8eb471db44cc
Nilesh <nileshjaat341@gmail.com>
2020-07-18 22:06:34 +0530

    implemented firebase utils, included ability to store users into our firestore database

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -35,7 +35,7 @@ class SignUp extends Component {
         password
       );
 
-      await createUserProfileDocument(user, displayName);
+      await createUserProfileDocument(user, { displayName });
 
       this.setState({
         displayName: "",
